057d42c2df7a6aa4ddafef64fa0875e0a5258578
Salomão Ribeiro <salomaomdm@gmail.com>
2023-10-02 07:07:47 -0300

    modulo 2 - criado UserController e método para salvar usuário

---
+++ Modulo02/src/app/controllers/UserController.js
@@ -0,0 +1,25 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    const usuarioExiste = await User.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (usuarioExiste)
+      return res.status(400).json({
+        error: 'user already exists',
+      });
+
+    const { id, nome, email, provider } = await User.create(req.body);
+
+    return res.json({
+      id,
+      nome,
+      email,
+      provider,
+    });
+  }
+}
+
+export default new UserController();
--- Modulo02/src/routes.js
+++ Modulo02/src/routes.js
@@ -1,18 +1,9 @@
 import { Router } from 'express';
 
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.update({
-    nome: 'salomao',
-    email: 'salomaomdm@gmail.com',
-    password_hash: '123',
-    provider: true,
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
