9aec9e10f7a742afde99d96dae3cd4331c927d6a
Salomão Ribeiro <salomaomdm@gmail.com>
2023-10-01 21:37:33 -0300

    modulo 2 - adicionando e confiurando sequelize

---
+++ Modulo02/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgress',
+  host: 'localhost',
+  user: 'postgres',
+  password: '12',
+  database: 'gobarber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
