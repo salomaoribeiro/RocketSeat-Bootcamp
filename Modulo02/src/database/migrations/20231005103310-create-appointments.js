dd1d99f17df0e66fede3b5a66e19c02797ac93e3
Salomão Ribeiro <salomaomdm@gmail.com>
2023-10-05 07:59:01 -0300

    modulo 3 - criado migration e model de appointment para os agendamentos

---
+++ Modulo02/src/database/migrations/20231005103310-create-appointments.js
@@ -0,0 +1,45 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('appointments', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      data: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'users', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true,
+      },
+      provider_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'users', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true,
+      },
+      canceled_at: {
+        type: Sequelize.DATE,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('appointments');
+  },
+};
