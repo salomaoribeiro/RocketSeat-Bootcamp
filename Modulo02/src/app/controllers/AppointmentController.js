104eb9059155673f676c081771988d8cddea3c31
Salomão Ribeiro <salomaomdm@gmail.com>
2023-10-06 07:11:13 -0300

    modulo 3 - instalado date-fns para manipular a data e criado verificacoes para appointment mesma data/hora

--- Modulo02/src/app/controllers/AppointmentController.js
+++ Modulo02/src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 
 import Appointment from '../models/Appointment';
 import User from '../models/User';
@@ -23,6 +24,22 @@ class AppointmentController {
       return res
         .status(401)
         .json({ error: 'You can only create appointment with a providers' });
+
+    const hourStart = startOfHour(parseISO(data));
+
+    if (isBefore(hourStart, new Date()))
+      return res.status(400).json({ error: 'Past dates are not permitted' });
+
+    const checkAvailability = Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        data: hourStart,
+      },
+    });
+
+    if (checkAvailability)
+      res.status(400).json({ error: 'Appointment date is not available' });
 
     const appointment = await Appointment.create({
       user_id: req.userId,
