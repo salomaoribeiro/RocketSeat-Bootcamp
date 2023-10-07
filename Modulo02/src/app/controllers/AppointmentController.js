0d0c23e8926511b447b8f40f5406d3d6a13b06d9
Salomão Ribeiro <salomaomdm@gmail.com>
2023-10-07 07:12:59 -0300

    modulo 3 - adicionado paginacao

--- Modulo02/src/app/controllers/AppointmentController.js
+++ Modulo02/src/app/controllers/AppointmentController.js
@@ -7,6 +7,8 @@ import User from '../models/User';
 
 class AppointmentController {
   async index(req, res) {
+    const { page = 1 } = req.query;
+
     const appointments = await Appointment.findAll({
       where: {
         user_id: req.userId,
@@ -14,6 +16,8 @@ class AppointmentController {
       },
       attributes: ['id', 'data'],
       order: ['data'],
+      limit: 20,
+      offset: (page - 1) * 20,
       include: [
         {
           model: User,
